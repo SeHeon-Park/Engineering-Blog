e142ce85f945e2dfb4e2c085546028dcf84f0857
seheon <quwe153@naver.com>
2022-06-23 14:48:19 +0900

    apply bootstrap

---
+++ project/src/main/java/jpa/blog/project/controller/BootStrapController.java
@@ -0,0 +1,13 @@
+package jpa.blog.project.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class BootStrapController {
+
+    @RequestMapping("/home")
+    public String home(){
+        return "index";
+    }
+}
--- project/src/main/java/jpa/blog/project/controller/HomeController.java
+++ project/src/main/java/jpa/blog/project/controller/HomeController.java
@@ -1,9 +1,15 @@
 package jpa.blog.project.controller;
 
 import jpa.blog.project.Entity.Member;
+import jpa.blog.project.Entity.ReviewSubject;
+import jpa.blog.project.Entity.Subject;
 import jpa.blog.project.Service.MemberService;
 import jpa.blog.project.form.MemberForm;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.User;
@@ -13,6 +19,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.Map;
+
 @Controller
 @RequiredArgsConstructor
 public class HomeController {
@@ -20,7 +28,7 @@ public class HomeController {
     private final MemberService memberService;
 
     @RequestMapping("/")
-    public String home(Model model){
+    public String home(Model model, @PageableDefault(page = 0, size = 2, sort = "id", direction = Sort.Direction.DESC) Pageable pageable){
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
         if(authentication.getPrincipal().toString() == "anonymousUser"){
             return "/home/home";
@@ -28,6 +36,10 @@ public class HomeController {
         User user = (User) authentication.getPrincipal();
         String uid = user.getUsername();
         Member member = memberService.findOneByUid(uid);
+
+        Map<Subject, Page<ReviewSubject>> reviewSubjects = memberService.findReviewSubjects(member.getMemberId(), pageable);
+        model.addAttribute("maps", reviewSubjects);
+
         MemberForm memberForm = new MemberForm(member.getMemberId(), member.getName());
         model.addAttribute("memberForm", memberForm);
         return "/home/userHome";
